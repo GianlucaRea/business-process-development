93015f7c5b8df0db4a6fa3711029063efea6d44d
Gianluca Rea <43826151+GianlucaRea@users.noreply.github.com>
2023-01-04 12:07:19 +0100

    update

--- User/User/src/main/java/it/univaq/disim/bpd/soap/UserEndpoint.java
+++ User/User/src/main/java/it/univaq/disim/bpd/soap/UserEndpoint.java
@@ -2,11 +2,13 @@ package it.univaq.disim.bpd.soap;
 
 import org.camunda.bpm.engine.RuntimeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 
 import it.univaq.disim.bpd.domain.FarmBusinessStartupResponseRequestType;
 
+@Endpoint
 public class UserEndpoint {
 
 	private static final String NAMESPACE_URI = "http://eu.chorevolution.farmbusinessstartup/user";
@@ -16,14 +18,12 @@ public class UserEndpoint {
 	
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "farmBusinessStartupResponseElementRequest")
 	public void receiveFarmBusinessCode(@RequestPayload FarmBusinessStartupResponseRequestType request) {
-		
+												  
 		System.out.println("Received SOAP message farmBusinessStartupResponseRequestType");
+		System.out.println("ChoreographyID :" + request.getChoreographyId().getChoreographyId());
+		System.out.println("Farm Code :" + request.getMessageData().getFarmCode());
 		
-
-		runtimeService.createMessageCorrelation("farmBusinessStartupResponseRequestType")
-				.processInstanceBusinessKey(request.getChoreographyId().getChoreographyId())
-				.setVariable("farmBusinessStartupResponseRequestType", request)
-				.correlate();
+		runtimeService.createMessageCorrelation("farmBusinessStartupResponseRequestType").processInstanceBusinessKey(request.getChoreographyId().getChoreographyId()).setVariable("farmBusinessStartupResponseRequestType", request).correlate(); 
 
 	}	
 }
